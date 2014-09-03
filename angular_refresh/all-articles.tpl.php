c5f7735c9188673f0162def69fcfe43f2d557e86
Gerald Villorente <geraldvillorente@gmail.com>
2014-09-03 22:50:32 +0800

    Remove hardcoded base url

--- angular_refresh/all-articles.tpl.php
+++ angular_refresh/all-articles.tpl.php
@@ -1,4 +1,4 @@
-<angular-refresh url="http://local.drupal-host.dev/nodes/json?callback=JSON_CALLBACK"
+<angular-refresh url="/nodes/json?callback=JSON_CALLBACK"
       ng-model="angularContent" interval="5000" method="jsonp"></angular-refresh>
 <div id="angular-content">
   <!-- Iterate the result. Equivalent to foreach in php. -->
