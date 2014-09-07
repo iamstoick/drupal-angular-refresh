463b378d781e543ba376af9612d0cad0486b75a9
Gerald Villorente <geraldvillorente@gmail.com>
2014-09-07 22:00:19 +0800

    Use track by to just update the specific part of the content instead of reloading the whole template

--- angular_refresh/all-articles.tpl.php
+++ angular_refresh/all-articles.tpl.php
@@ -2,7 +2,7 @@
       ng-model="angularContent" interval="5000" method="jsonp"></angular-refresh>
 <div id="angular-content">
   <!-- Iterate the result. Equivalent to foreach in php. -->
-  <div ng-repeat="item in angularContent.nodes">
+  <div ng-repeat="item in angularContent.nodes track by item.node.Nid">
     <h2>{{item.node.title}}</h2>
     <div class="body">{{item.node.Body}}</div>
     <div class="image"><img src="{{item.node.Image}}" /></div>
